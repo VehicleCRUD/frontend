20b9f6994f8e019c1ff5cc72f0bcda82857a5a5e
Edgar Andre Anco Copaja <u201724411@upc.edu.pe>
2023-10-22 11:28:10 -0500

    feat(vehicle): add vehicle display component

---
+++ src/app/vehicle-display/vehicle-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-display',
+  templateUrl: './vehicle-display.component.html',
+  styleUrls: ['./vehicle-display.component.css']
+})
+export class VehicleDisplayComponent {
+
+}
